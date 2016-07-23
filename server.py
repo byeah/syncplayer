03228de9090f4eeac5c7c130f2367acb78f2cb6e
ZYShin <yeshuangzhu@gmail.com>
2016-07-23 18:25:13 +0800

    Sync commands implemented!

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
 from flask.ext.socketio import SocketIO, emit
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+
+epoch = datetime.utcfromtimestamp(0)
+delay = 3000    # milliseconds
 
 @app.route('/')
 def index():
@@ -19,7 +23,10 @@ def test_message(message):
 
 @socketio.on('video control', namespace='/test')
 def test_message(message):
-    emit('video control', {'data': message['data'],'name':message['name']}, broadcast=True)
+    params = {'actiontype': message['actiontype'], 'progress': message['progress']}
+    if message['actiontype'] == 'play':
+        params['ontime'] = int((datetime.utcnow() - epoch).total_seconds() * 1000) + delay
+    emit('video control', params, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app)
