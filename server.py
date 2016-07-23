2c8c1b9b789026c0391415e8190db45cc6cbcd9f
byeah <jby.yeah@gmail.com>
2016-07-23 14:17:44 +0900

    some fix

--- server.py
+++ server.py
@@ -11,23 +11,15 @@ def index():
 
 @socketio.on('my event', namespace='/test')
 def test_message(message):
-    emit('my response', {'data': message['data']})
+    emit('my response', {'name':'Server','data': message['data']})
 
 @socketio.on('my broadcast event', namespace='/test')
 def test_message(message):
-    emit('my response', {'data': message['data']}, broadcast=True)
+    emit('my response', {'data': message['data'],'name':message['name']}, broadcast=True)
 
 @socketio.on('video control', namespace='/test')
 def test_message(message):
-    emit('video control', {'data': message['data']}, broadcast=True)
-
-@socketio.on('connect', namespace='/test')
-def test_connect():
-    emit('my response', {'data': 'Connected'})
-
-@socketio.on('disconnect', namespace='/test')
-def test_disconnect():
-    print('Client disconnected')
+    emit('video control', {'data': message['data'],'name':message['name']}, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app)
